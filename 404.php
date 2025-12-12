431d66a4d0709e9794bcad0a479d4bff3f4ab056
Rifas Bodegom Unifam <rifaselbodegomunifam@gmail.com>
2025-12-12 06:48:00 -0500

    Add changes for the development theme

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+
+use \Juztstack\JuztStudio\Community\Templates;
+use Timber\Timber;
+
+$template = new Templates();
+$template_content = $template->get_json_template('404');
+
+$context = Timber::context();
+$context['order'] = $template_content['order'];
+$context['sections'] = $template_content['sections'];
+
+Timber::render('templates/index.twig', $context); 
